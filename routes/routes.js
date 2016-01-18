debdc90678928f6d5415c65fdb04174c35909ed9
Victor Tang <victortang1992@gmail.com>
2016-01-17 22:21:54 -0800

    just use hashid, not really shorter lol

--- routes/routes.js
+++ routes/routes.js
@@ -2,50 +2,36 @@
 
 //only works for numbers, no strings
 var Hashids = require('hashids'),
-    hashids = new Hashids("this is my salt");
-
-//what you really want is:
-
-
+    hashids = new Hashids("hi");
 
 module.exports = function(app) {
 
     // =====================================
     // HOME PAGE (with login links) ========
     // =====================================
+	function numberfromURL(input){
+		return input.split("").map(function(letter){return letter.charCodeAt(0);});
+	}
 	function generateShort(input){
-		//only works for numbers, no strings
-		var id = hashids.encode(12345);
-		return id;
-		//what you really want is:
-		
+		//only works for numbers or array of nums, no strings
+		return hashids.encode(numberfromURL(input));
+	}
+	function decode(input){
+		var arr = hashids.decode(input);
+		return arr.map(function(num){return String.fromCharCode(num);}).join("");
 	}
     app.get('/', function(req, res) {
-		console.log("got '/'")
         res.render('index.ejs'); // load the index.ejs file
     });
-	app.get('/new/https://:url', function (req, res) {
-		var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-		
-		var urlToShorten = "https://"+req.params.url;
-		var shortCode = generateShort(urlToShorten);
+	app.get('/new/https://:url', function (req, res) {		
+		var urlToEncode = req.params.url;
+		var shortCode = generateShort(urlToEncode);
 		var results = { "original_url": "https://"+req.params.url, "short_url": req.protocol + '://' + req.get('host') + '/' + shortCode }
-		
 		res.send(results)
 		});
-	
-	app.post('/api/whoami', function(req, res) {
-		var langCapt = /([a-z]{2}-[A-Z]{2})/g;
-		var softCapt = /\((.*)\)/g;
-		var returnvalue = {
-			"ipaddress": req.ip ||
-					 req.connection.remoteAddress || 
-					 req.socket.remoteAddress ||
-					 req.connection.socket.remoteAddress ||
-					req.headers['x-forwarded-for'],
-			"language": req.headers['accept-language'].match(langCapt)[0],
-			"software": softCapt.exec(req.headers['user-agent'])[1],
-		}
-        res.send(JSON.stringify(returnvalue)); // send STRING with JSON data inside
-    });
+	app.get('/:code', function (req, res) {
+		var urlToDecode = req.params.code;
+		var origURL = decode(urlToDecode);
+		res.redirect("https://"+origURL)
+		});
 }
