99403336962f2d9211c0b5966c7084c9716efd15
Victor Tang <victortang1992@gmail.com>
2016-01-17 23:54:36 -0800

    make generator in "/" as text input

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@
 app.controller('MainCtrl', ['$scope', '$http', '$window', function($scope, $http, $window){
 	$scope.shortURL;
 	$scope.generateShort = function(){
-		
+		$http.post($window.location.href+"new/http://"+$scope.userInputURL).success(function(data){$scope.shortURL=data;}).error(function(err){throw err;});
 	};
 }]);//end of controller
   //end of function
--- routes/routes.js
+++ routes/routes.js
@@ -20,18 +20,21 @@ module.exports = function(app) {
 		var arr = hashids.decode(input);
 		return arr.map(function(num){return String.fromCharCode(num);}).join("");
 	}
+	function getURLinfo(req, res){
+		var urlToEncode = req.params.url;
+		var shortCode = generateShort(urlToEncode);
+		var results = { "original_url": "http://"+req.params.url, "short_url": req.protocol + '://' + req.get('host') + '/' + shortCode }
+		res.send(JSON.stringify(results));
+	}
     app.get('/', function(req, res) {
         res.render('index.ejs'); // load the index.ejs file
     });
-	app.get('/new/https://:url', function (req, res) {		
-		var urlToEncode = req.params.url;
-		var shortCode = generateShort(urlToEncode);
-		var results = { "original_url": "https://"+req.params.url, "short_url": req.protocol + '://' + req.get('host') + '/' + shortCode }
-		res.send(results)
-		});
+	app.get('/new/https://:url', function(req, res){getURLinfo(req, res)});
+	app.get('/new/http://:url', function(req, res){getURLinfo(req, res)});
+	app.post('/new/http://:url', function(req, res){getURLinfo(req, res)});
 	app.get('/:code', function (req, res) {
 		var urlToDecode = req.params.code;
 		var origURL = decode(urlToDecode);
-		res.redirect("https://"+origURL)
+		res.redirect("http://"+origURL)
 		});
 }
